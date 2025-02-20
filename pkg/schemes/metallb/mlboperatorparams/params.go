53dfbbeb3ccbe160e89b2136e420d335a2637ac8
kononovn <10511306+kononovn@users.noreply.github.com>
2025-02-20 01:21:29 +0000

    sync: update external libs

--- pkg/schemes/metallb/mlboperatorparams/params.go
+++ pkg/schemes/metallb/mlboperatorparams/params.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/Masterminds/semver"
 	v1beta1 "github.com/openshift-kni/eco-goinfra/pkg/schemes/metallb/mlboperator"
 )
 
@@ -15,23 +16,28 @@ type ImageInfo struct {
 	Tag  string
 }
 
-func BGPType(m *v1beta1.MetalLB, isOpenshift bool) v1beta1.BGPType {
-	if m.Spec.BGPBackend == "" {
-		if isOpenshift {
-			return v1beta1.FRRK8sMode
-		}
-		return v1beta1.FRRMode
+func BGPType(m *v1beta1.MetalLB, env EnvConfig) v1beta1.BGPType {
+	if m.Spec.BGPBackend != "" {
+		return m.Spec.BGPBackend
 	}
-	return m.Spec.BGPBackend
+	if env.IsOpenshift && env.MustDeployFRRK8sFromCNO {
+		return v1beta1.FRRK8sExternalMode
+	}
+	if env.IsOpenshift {
+		return v1beta1.FRRK8sMode
+	}
+	return v1beta1.FRRMode
 }
 
 type EnvConfig struct {
 	Namespace                  string
+	FRRK8sExternalNamespace    string
 	ControllerImage            ImageInfo
 	SpeakerImage               ImageInfo
 	FRRImage                   ImageInfo
 	FRRK8sImage                ImageInfo
 	KubeRBacImage              ImageInfo
+	CNOMinFRRK8sVersion        string
 	MLBindPort                 int
 	FRRMetricsPort             int
 	SecureFRRMetricsPort       int
@@ -44,6 +50,7 @@ type EnvConfig struct {
 	DeployPodMonitors          bool
 	DeployServiceMonitors      bool
 	IsOpenshift                bool
+	MustDeployFRRK8sFromCNO    bool
 }
 
 func FromEnvironment(isOpenshift bool) (EnvConfig, error) {
@@ -121,8 +128,14 @@ func FromEnvironment(isOpenshift bool) (EnvConfig, error) {
 		res.DeployServiceMonitors = true
 	}
 
+	res.FRRK8sExternalNamespace = os.Getenv("FRRK8S_EXTERNAL_NAMESPACE")
+	if os.Getenv("DEPLOY_FRRK8S_FROM_CNO") == "true" {
+		res.MustDeployFRRK8sFromCNO = true
+	}
+
 	// Ignoring the error, if not set we'll consume the image from the chart
 	res.FRRK8sImage, _ = imageFromEnv("FRRK8S_IMAGE")
+	res.CNOMinFRRK8sVersion = os.Getenv("CNO_MIN_FRRK8S_VERSION")
 
 	err = validate(res)
 	if err != nil {
@@ -141,6 +154,12 @@ func validate(config EnvConfig) error {
 	}
 	if config.SecureFRRMetricsPort != 0 && !config.DeployServiceMonitors {
 		return fmt.Errorf("secureFRRMetricsPort is available only if service monitors are enabled")
+	}
+	if config.CNOMinFRRK8sVersion != "" {
+		_, err := semver.NewVersion(config.CNOMinFRRK8sVersion)
+		if err != nil {
+			return fmt.Errorf("invalid cno min frrk8s supported version: %w", err)
+		}
 	}
 	return nil
 }
